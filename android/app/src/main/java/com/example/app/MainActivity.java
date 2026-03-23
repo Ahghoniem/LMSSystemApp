1672c052876bb9e637a2e846755f62ce2a3eb98e
Ahghoniem <ahmedghona054@gmail.com>
2026-03-23 20:21:35 +0200

    Configure Capacitor settings and MainActivity

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,15 @@
 package com.example.app;
-
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // Allow HTTPS page to load HTTP API
+    this.bridge.getWebView()
+      .getSettings()
+      .setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+  }
+}
